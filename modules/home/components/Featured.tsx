4c201b4568d135c763e4677b855620070f14ef60
Onlyadmirer <akmalrbc6@gmail.com>
2026-05-30 13:53:31 +0800

    feat: add icons social to featured card contact

--- modules/home/components/Featured.tsx
+++ modules/home/components/Featured.tsx
@@ -3,13 +3,13 @@
 import SubHeaderSection from "@/common/components/elements/SubHeaderSection";
 import SpotlightCard from "@/common/components/ui/SpotlightCard";
 import Link from "next/link";
-import { GrTechnology } from "react-icons/gr";
-import { LuBookUser, LuLayoutDashboard, LuUser } from "react-icons/lu";
+import { LuLayoutDashboard } from "react-icons/lu";
 import { MdBackupTable } from "react-icons/md";
 import CardAchiev from "./featured-components/CardAchiev";
 import CardProject from "./featured-components/CardProject";
 import CardAbout from "./featured-components/CardAbout";
 import CardSkills from "./featured-components/CardSkills";
+import CardContact from "./featured-components/CardContact";
 
 function Featured() {
   return (
@@ -29,29 +29,7 @@ function Featured() {
         {/* Card Achievements */}
         <CardAchiev />
         {/* Card Contact */}
-        <SpotlightCard
-          className=' h-80'
-          spotlightColor='rgba(200, 0, 100, 0.2)'
-        >
-          <Link href={"/contact"}>
-            <div className=' flex items-center flex-col gap-2'>
-              <div className='p-3 rounded-xl h-14 w-14 bg-neutral-300 dark:bg-neutral-800'>
-                <LuBookUser
-                  size={24}
-                  className='object-cover w-full h-full text-description'
-                />
-              </div>
-              <div className='text-center'>
-                <h2 className='text-description text-sm font-medium'>
-                  Contact
-                </h2>
-                <p className='text-neutral-400 tracking-tight text-xs'>
-                  Reach out via email, GitHub, or LinkedIn.
-                </p>
-              </div>
-            </div>
-          </Link>
-        </SpotlightCard>
+        <CardContact />
         {/* Card Dashboard */}
         <SpotlightCard
           className=' h-80 md:col-span-2'
--- modules/home/components/featured-components/CardContact.tsx
+++ modules/home/components/featured-components/CardContact.tsx
@@ -1,5 +1,40 @@
+"use client";
+
+import SpotlightCard from "@/common/components/ui/SpotlightCard";
+import Link from "next/link";
+import { LuBookUser } from "react-icons/lu";
+import InstagramIcon from "@iconify-react/skill-icons/instagram";
+import GmailDarkIcon from "@iconify-react/skill-icons/gmail-dark";
+import LinkedinIcon from "@iconify-react/skill-icons/linkedin";
+import GithubLightIcon from "@iconify-react/skill-icons/github-light";
+
 function CardContact() {
-  return <div>CardContact</div>;
+  return (
+    <SpotlightCard className=' h-80' spotlightColor='rgba(200, 0, 100, 0.2)'>
+      <Link href={"/contact"}>
+        <div className=' flex items-center flex-col gap-2'>
+          <div className='p-3 rounded-xl h-14 w-14 bg-neutral-300 dark:bg-neutral-800'>
+            <LuBookUser
+              size={24}
+              className='object-cover w-full h-full text-description'
+            />
+          </div>
+          <div className='text-center'>
+            <h2 className='text-description text-sm font-medium'>Contact</h2>
+            <p className='text-neutral-400 tracking-tight text-xs'>
+              Reach out via email, GitHub, or LinkedIn.
+            </p>
+          </div>
+        </div>
+      </Link>
+      <div className='grid grid-cols-2 mt-8 space-y-8 justify-items-center m-auto w-full'>
+        <InstagramIcon className='h-10 hover:-rotate-6 hover:scale-110 transition-all duration-300 ease-in-out' />
+        <GmailDarkIcon className='h-10 hover:rotate-6 hover:scale-110 transition-all duration-300 ease-in-out' />
+        <LinkedinIcon className='h-10 hover:rotate-6 hover:scale-110 transition-all duration-300 ease-in-out' />
+        <GithubLightIcon className='h-10 hover:-rotate-6 hover:scale-110 transition-all duration-300 ease-in-out' />
+      </div>
+    </SpotlightCard>
+  );
 }
 
 export default CardContact;
