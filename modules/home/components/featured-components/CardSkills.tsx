65a815a097aaec9119493b28c2f82806c8ea2c84
Onlyadmirer <akmalrbc6@gmail.com>
2026-05-30 14:44:19 +0800

    feat: update height logo loop

--- modules/home/components/featured-components/CardSkills.tsx
+++ modules/home/components/featured-components/CardSkills.tsx
@@ -45,10 +45,15 @@ export default function CardSkills() {
           </div>
         </div>
       </Link>
-      <div className='h-96 md:h-40 mt-8 space-y-8 md:space-y-2 relative overflow-hidden'>
+      <div className='h-96 md:h-40 mt-12 md:mt-8 space-y-8 md:space-y-2 relative overflow-hidden'>
         {/* Vertical loop with deceleration on hover */}
-        <LogoLoop speed={50} logos={techLogos} />
-        <LogoLoop speed={50} direction='right' logos={techLogos} />
+        <LogoLoop speed={50} logoHeight={36} logos={techLogos} />
+        <LogoLoop
+          speed={50}
+          logoHeight={36}
+          direction='right'
+          logos={techLogos}
+        />
       </div>
     </SpotlightCard>
   );
