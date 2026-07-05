62f559f5623bff80660821e2fb9911230bfd9205
Onlyadmirer <akmalrbc6@gmail.com>
2026-07-05 10:07:35 +0800

    feat: add animation after render

--- modules/home/Home.tsx
+++ modules/home/Home.tsx
@@ -1,14 +1,23 @@
+"use client";
+
+import ChatShortcut from "../guestbook/components/ChatShortcut";
 import Featured from "./components/Featured";
 import Header from "./components/Header";
 import Skils from "./components/Skils";
+import { motion } from "motion/react";
 
 function Home() {
   return (
-    <div>
+    <motion.div
+      initial={{ opacity: 0, y: 50 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.4, ease: "easeInOut" }}
+    >
       <Header />
       <Skils />
       <Featured />
-    </div>
+      <ChatShortcut />
+    </motion.div>
   );
 }
 
