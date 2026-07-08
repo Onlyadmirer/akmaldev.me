53a4039faefb09f9e03ec2014bde469734fca9aa
Onlyadmirer <akmalrbc6@gmail.com>
2026-07-08 11:31:18 +0800

    feat: add ad checks

--- modules/dashboard/components/GithubStats.tsx
+++ modules/dashboard/components/GithubStats.tsx
@@ -28,7 +28,7 @@ function GithubStats() {
   }
 
   return (
-    <div className='py-6 flex flex-col gap-4'>
+    <div className='py-6 flex flex-col gap-4 border-b border-neutral-600'>
       <div className='flex flex-row justify-between items-end'>
         <SubHeaderSection
           icon={<GithubIcon height='2em' />}
