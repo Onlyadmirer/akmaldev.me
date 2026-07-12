68cc779b2b01efbad0fc27732e57836595a862d5
Onlyadmirer <akmalrbc6@gmail.com>
2026-07-12 11:51:32 +0800

    fix: change useRouter next/ to i18n

--- common/layouts/navbar/useNavbar.tsx
+++ common/layouts/navbar/useNavbar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { usePathname } from "next/navigation";
+import { usePathname } from "@/i18n/navigation";
 import { useState } from "react";
 import { IoChatbubbleEllipsesOutline } from "react-icons/io5";
 import {
